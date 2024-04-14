aeffa1f3e2bd13e6f0181fe54570789cd48878cd
Bhawani kumar <159748112+bhawanikumar2003@users.noreply.github.com>
2024-04-15 01:20:16 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>I'm Bhawani</span> I am a Full-Stack developer based in Bihar, India. I am an Information CSA from BCA.<br><br> I am very passionate about improving my coding skills and developing applications and websites. I build WebApps and Websites. Working for myself to improve my skills. Love to build Full-Stack clones."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
